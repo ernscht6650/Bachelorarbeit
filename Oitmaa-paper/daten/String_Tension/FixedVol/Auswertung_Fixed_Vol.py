7df069e66746099380c3dc529a27e0bcbbddda53
ernscht6650 <florian.stein.22@gmx.de>
2025-06-05 22:32:52 +0200

    ST Auswertung

--- Oitmaa-paper/daten/String_Tension/FixedVol/Auswertung_Fixed_Vol.py
+++ Oitmaa-paper/daten/String_Tension/FixedVol/Auswertung_Fixed_Vol.py
@@ -47,7 +47,7 @@ x1=np.linspace(0.05,0.5,1000)
 
 plt.plot(x,x**2, color='black')
 ls=[0.05, 0.1, 0.2, 0.3, 0.4, 0.45, 0.475, 0.485, 0.495]
-Ms=[0, 0.05, 0.1, 0.2, 0.4, 0.8, 1.6]
+Ms=[0, 0.05, 0.1, 0.2,0.3, 0.35, 0.4, 0.8, 1.6]
 STs=np.zeros((len(Ms), len(ls)))
 Err=np.zeros((len(Ms), len(ls)))
 STs2=np.zeros((len(Ms), len(ls)))
@@ -56,8 +56,8 @@ Err2=np.zeros((len(Ms), len(ls)))
 plt.gca().set_prop_cycle(None)
 for i in range(0,len(Ms)):
     for j in range(0,len(ls)):
-        #print(str(Ms[i]), str(ls[j]))
-        STs[i][j],Err[i][j]=Extrapolator(str(Ms[i]), str(ls[j]),3, 0)
+        print(str(Ms[i]), str(ls[j]))
+        STs[i][j],Err[i][j]=Extrapolator(str(Ms[len(Ms)-1-i]), str(ls[j]),3, 0)
 #       STs2[i][j],Err2[i][j]=Extrapolator(str(Ms[i]), str(ls[j]),3, 0)
 
     #plt.plot(ls, STs[i,:], linestyle='--', marker='.', markersize=10,  label=str(Ms[i]))#linewidth=1,
@@ -70,13 +70,13 @@ for i in range(0,len(Ms)):
 
 plt.gca().set_prop_cycle(None)
 for i in range(0,len(Ms)):
-    plt.plot(-5,0.1, marker='.', linestyle='', markersize=8, label=str(Ms[i]))
+    plt.plot(-5,0.1, marker='.', linestyle='', markersize=8, label=str(Ms[len(Ms)-1-i]))
 
 #plt.plot(x,0*x, color='black')
 plt.xlim(0,0.5)
 plt.ylim(-0.01,0.25)
 
-plt.legend(loc="upper left",handletextpad=-0.5, borderpad=0.3)
+plt.legend(loc="upper left",handletextpad=-0.5, borderpad=0.4)
 
 plt.xlabel('$l_0$',  fontsize=17)
 plt.ylabel('$\\frac{2T}{g^2}$', fontsize=17, rotation=0)
