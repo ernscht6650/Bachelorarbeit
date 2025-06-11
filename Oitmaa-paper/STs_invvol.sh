643db18573914a0fe10123f230734ce552115f16
ernscht6650 <florian.stein.22@gmx.de>
2025-06-11 10:47:42 +0200

    mehr renormierunge

--- Oitmaa-paper/STs_invvol.sh
+++ Oitmaa-paper/STs_invvol.sh
@@ -1,7 +1,13 @@
-for l in 0.55 0.6 0.7 0.8 0.9 0.95
+for l in  0.6 0.7 0.8 0.9 0.95
 do
-python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10)" >> MS_AllesMoegliche_N_y_l.dat &
-sleep 50m
+python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 85, 150)" >> MS_AllesMoegliche_N_y_l.dat &
+sleep 80m
+done
+
+for l in 0.05 0.1 0.2 0.3 0.4 0.45 0.55
+do
+python3 -c "from OitmaaV2 import *; ComputeMassShift(${l},24,10, 85, 95)" >> MS_AllesMoegliche_N_y_l.dat &
+sleep 15m
 done
 
 sleep 200m
@@ -10,8 +16,8 @@ for m in  0.3 0.35 0.4 0.8 1.6
 do
 for l in 0.6 0.7 0.8 0.9 0.95
 do
-python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l})" >> STV2_m${m}_l${l}.dat &
-sleep 30m
+python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l}, 85, 150)" >> ./daten/String_Tension/STV2_m${m}_l${l}.dat &
+sleep 35m
 done
 done
 
@@ -20,7 +26,26 @@ for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
 do
 for l in 0.55
 do
-python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l})" >> STV2_m${m}_l${l}.dat &
-sleep 30m
+python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l}, 85, 150)" >> ./daten/String_Tension/STV2_m${m}_l${l}.dat &
+sleep 35m
+done
+done
+
+for m in  0 0.05 0.1 0.2
+do
+for l in 0.6 0.7 0.8 0.9 0.95 
+do
+python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l}, 85, 95)" >> ./daten/String_Tension/STV2_m${m}_l${l}.dat &
+sleep 8m
+done
+done
+
+
+for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
+do
+for l in 0.05 0.1 0.2 0.3 0.4 0.45 
+do
+python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l}, 85, 95)" >> ./daten/String_Tension/STV2_m${m}_l${l}.dat &
+sleep 8m
 done
 done
--- Oitmaa-paper/test.sh
+++ Oitmaa-paper/test.sh
@@ -1,9 +1,9 @@
-for l in 0.3 0.4
+for m in  0.2 0.35 0.4 0.8 1.6
 do
-for m in 0.1 0.2
+for l in 0.49 0.7 0.8 0.9 0.95
 do
-echo $m, $l
-python3 -c "from OitmaaV2 import *; ComputeStringtensionVol(25,${m},${l},10,10)" >> Test_m${m}_l${l}.dat &
+python3 -c "from OitmaaV2 import *; ComputeStringtension(${m}, ${l}, 100, 150)" >> ./daten/String_Tension/STV2_m${m}_l${l}.dat &
+sleep 30m
 done
-sleep 3
 done
+
