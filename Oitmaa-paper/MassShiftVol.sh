ce1c2f3ee60ec359af3ea1554fee3f098c825abf
ernscht6650 <florian.stein.22@gmx.de>
2025-06-11 23:39:38 +0200

    Stand der dinge Godot

--- Oitmaa-paper/MassShiftVol.sh
+++ Oitmaa-paper/MassShiftVol.sh
@@ -4,12 +4,12 @@ VOL=$1
 Nmin=$3
 Nmax=$4
 
-for L in 0.225 0.25 0.275 0.3 0.325 0.35 0.375 0.4 0.425 0.45 0.475
+for L in 0.25 0.05 0.075 0.1 0.125 0.15 0.175 0.2 0.225 0.25 0.275 0.3 0.325 0.35 0.375 0.4 0.425 0.45 0.475
 do
 
 for N in $(seq ${Nmin} 2 ${Nmax})
 do
 python3 -c "from NonDecoOitmaa import *; print('\"'+str(${VOL})+'_'+str(${N})+'_'+str(${L})+'\": ', MassShift(${N}, ${VOL}/${N}, ${L}, -0.135*${VOL}/${N}, 0.02), end=',\n')" >> MS_test.dat&
 done
-sleep 5m
+sleep 300s
 done
--- Oitmaa-paper/Neue_MassShifts.sh
+++ Oitmaa-paper/Neue_MassShifts.sh
@@ -1,39 +1,36 @@
-for l in 0.15 0.25 0.35 0.65 0.75 0.85
+for l in 0.15 0.25 0.35 0.225 0.275 0.075 0.325
 do 
-python3 -c "from OitmaaV2 import *; MassShiftVol(25,${l},26,10)" >> MS_Vol.dat &
-sleep 17m
+python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 85, 150):" >> MS_AllesMoegliche_N_y_l.dat &
+sleep 90m
 done
 
+for l in 0.05 0.025
+do 
+python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 80, 96):" >> MS_AllesMoegliche_N_y_l.dat &
+sleep 25m
+done
 
+sleep 30m
+
+
+
+
+#for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
+#do
+#for l in 0.515 0.525 0.95 0.55 0.8 0.8 0.7 0.6 0.15 0.25 0.35 0.65 0.75 0.85
+#do
+#python3 -c "from OitmaaV2 import *; ComputeCondensate(25,${m}, ${l}, 10,26)" >> CC_Vol25_m${m}_l${l}.dat &
+#sleep 4m
+#done
+#done
 
 
 for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
 do
-for l in 0.515 0.525 0.95 0.55 0.8 0.8 0.7 0.6 0.15 0.25 0.35 0.65 0.75 0.85
-do
-python3 -c "from OitmaaV2 import *; ComputeCondensate(25,${m}, ${l}, 10,26)" >> CC_Vol25_m${m}_l${l}.dat &
-sleep 4m
-done
-done
-
-
-for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
-do
-for l in 0.05 0.1 0.2 0.3 0.4 0.45 0.475 0.485 0.49 0.55 0.6 0.7 0.8 0.9 0.95
+for l in 0.05 0.1 0.2 0.3 0.4 0.45 0.475 0.485 0.49
 do
 python3 -c "from OitmaaV2 import *; ComputeCondensateV2(${m}, ${l},10,24)" >> CC_V2_m${m}_l${l}.dat &
 sleep 20m
 done
 done
-
-
-for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
-do
-for l in 0.15 0.25 0.35 0.65 0.75 0.85
-do
-python3 -c "from OitmaaV2 import *; ComputeStringtensionVol(25,${m}, ${l}, 10, 26)">> ST_Vol25_m${m}_l${l}.dat &
-sleep 4m
-done
-done
-
 
