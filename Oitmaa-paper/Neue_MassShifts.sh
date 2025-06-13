f2f00961ca2abf49e749c01b739f1af9746813b0
ernscht6650 <florian.stein.22@gmx.de>
2025-06-13 20:23:02 +0200

    GSD skripte

--- Oitmaa-paper/Neue_MassShifts.sh
+++ Oitmaa-paper/Neue_MassShifts.sh
@@ -1,19 +1,18 @@
-sleep 120m
-for l in 0.175 0.075 0.125 
-do 
-bash MassShiftV2.sh ${l} 10 24 85 150
+#sleep 120m
+#for l in 0.175 0.075 0.125 
+#do 
+#bash MassShiftV2.sh ${l} 10 24 85 150
 #python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 85, 150)" >> MS_AllesMoegliche_N_y_l.dat &
-sleep 100m
-done
+#sleep 100m
+#done
 
-for l in 0.05 
-do
-bash MassShiftV2.sh ${l} 10 24 85 95 
+#for l in 0.05 
+#do
+#bash MassShiftV2.sh ${l} 10 24 85 95 
 #python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 80, 96):" >> MS_AllesMoegliche_N_y_l.dat &
-sleep 35m
-done
-
-sleep 30m
+#sleep 35m
+#done
+#sleep 30m
 
 
 
@@ -27,13 +26,23 @@ sleep 30m
 #done
 #done
 
+for m in   0.3 
+do
+for l in 0.2 0.3 0.4 0.45
+do
+python3 -c "from OitmaaV2 import *; ComputeCondensateV2(${m}, ${l},10,24)" >> CC_V2_m${m}_l${l}.dat &
+sleep 45m
+done
+done
 
-for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
+
+
+for m in   0.3 0.35 0.4 0.8 1.6
 do
 for l in 0.05 0.1 0.2 0.3 0.4 0.45
 do
 python3 -c "from OitmaaV2 import *; ComputeCondensateV2(${m}, ${l},10,24)" >> CC_V2_m${m}_l${l}.dat &
-sleep 30m
+sleep 45m
 done
 done
 
---
+++ Oitmaa-paper/StringtensionVol.py
@@ -0,0 +1,28 @@
+\#!/bin/bash
+
+VOL=$1
+Nmin=$2
+Nmax=$2
+
+for  l in 0 0.1 0.2 0.3 0.4 0.45
+do
+for m in 0 0.05 0.1 0.2 0.35 0.4 0.8 1.6
+do
+python3 << EOF >> eder.txt
+import ast
+Vol=${VOL}
+mdurchg=${m}
+l0=${l}
+
+with open('Grundzustands_dictionary_Vol.dat') as f:
+    data = f.read()
+dictVol = ast.literal_eval('{'+data+'}')
+
+def GZDVol(Vol, N, mdurchg, l0):
+        return dictVol[str(Vol)+"_"+str(N)+"_"+str(l0)+"_"+str(mdurchg)]
+
+for N in range(${Nmin}, ${Nmax}+1,2):
+	print(N, Vol, f"{mdurchg:3.2f}", f"{l0:3.2f}", f"{GZDVol(Vol, N, mdurchg, 0):15.13f}", f"{GZDVol(Vol, N, mdurchg, l0):15.13f}", f"{GZDVol(Vol, N, mdurchg, l0)-GZDVol(Vol, N, mdurchg, 0):15.13f}")
+EOF
+done
+done
