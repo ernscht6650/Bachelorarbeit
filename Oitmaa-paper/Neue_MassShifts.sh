013d5e25344dbc6c6ad0589e322168ba6d59bb3b
ernscht6650 <florian.stein.22@gmx.de>
2025-06-13 11:36:02 +0200

    mehr MS infvol und paar Efelder

--- Oitmaa-paper/Neue_MassShifts.sh
+++ Oitmaa-paper/Neue_MassShifts.sh
@@ -1,16 +1,19 @@
-for l in 0.15 0.25 0.35 0.225 0.275 0.075 0.325
+sleep 120m
+for l in 0.175 0.075 0.125 
 do 
-python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 85, 150)" >> MS_AllesMoegliche_N_y_l.dat &
+bash MassShiftV2.sh ${l} 10 24 85 150
+#python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 85, 150)" >> MS_AllesMoegliche_N_y_l.dat &
 sleep 100m
 done
 
-#for l in 0.05 0.025
-#do 
+for l in 0.05 
+do
+bash MassShiftV2.sh ${l} 10 24 85 95 
 #python3 -c "from OitmaaV2 import *; ComputeMassShift(${l}, 24, 10, 80, 96):" >> MS_AllesMoegliche_N_y_l.dat &
-#sleep 35m
-#done
+sleep 35m
+done
 
-#sleep 30m
+sleep 30m
 
 
 
@@ -27,7 +30,7 @@ done
 
 for m in  0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
 do
-for l in 0.05 0.1 0.2 0.3 0.4 0.45 0.475 0.485 0.49
+for l in 0.05 0.1 0.2 0.3 0.4 0.45
 do
 python3 -c "from OitmaaV2 import *; ComputeCondensateV2(${m}, ${l},10,24)" >> CC_V2_m${m}_l${l}.dat &
 sleep 30m
