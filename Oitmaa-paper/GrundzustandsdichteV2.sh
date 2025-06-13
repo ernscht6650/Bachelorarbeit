e9dafedadb4791dc85f1aa29ed23fa9bebcd0b81
ernscht6650 <florian.stein.22@gmx.de>
2025-06-13 12:49:25 +0200

    gZV2 skript

---
+++ Oitmaa-paper/GrundzustandsdichteV2.sh
@@ -0,0 +1,14 @@
+Nmin=$1
+Nmax=$2
+
+for mdurchg in 0 0.05 0.1 0.2 0.3 0.35 0.4 0.8 1.6
+do
+    for eta in $(seq 100 5 150)
+    do
+    for N in $(seq ${Nmin} 2 ${Nmax})
+    do
+        python3 -c "from NonDecoOitmaa import *; print(${mdurchg}, 0,  ${eta}/100, ${N}, GrundzustandsenergieV2(${N}, ${eta}/100, ${mdurchg}))">> GZ_DichteV2_m${mdurchg}_l0.dat &
+    done
+   sleep 2m
+   done
+done
--- Oitmaa-paper/MS_extrapolation.py
+++ Oitmaa-paper/MS_extrapolation.py
@@ -2,6 +2,7 @@ import ast
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 with open('MS_test.dat') as f:
     data = f.read()
 dictVol = ast.literal_eval('{'+data+'}')
@@ -27,6 +28,9 @@ def extrapolVol(Vol,N, Ls, plot=0):
 
 x=np.linspace(0,0.5,1000)
 
+
+colors=['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
+
 def extrapolVolume():
     Ls=np.array(range(25,476,25))/1000
     Ls2=np.array(range(25,476,25))/1000
@@ -42,29 +46,44 @@ def extrapolVolume():
     plt.legend(loc="lower left",handletextpad=-0.5, borderpad=0.4)
     plt.show()
 
-def extrapolInfvol(N,y, Ls, plot=0):
+def extrapolInfvol(N,y, Ls, plot=0, mogeln=0):
     MSs=[]
     for l in Ls:
-        MSs.append(Renormierung(N,y,l)/y)
+        if l >= 0.25 and mogeln==1:
+            MSs.append(0.25-Renormierung(N,y,round(0.5-l,4))/y)
+        else:
+            MSs.append(Renormierung(N,y,l)/y)
     if plot == 1:
-        plt.plot(Ls, MSs, linestyle=' ', marker='.', markersize=10, label=str(N)+", "+str(y))
+        plt.plot(Ls, MSs, linestyle=' ', marker='.', markersize=10, label=str(N)+", "+str(y), color=colors[i])
     return MSs
 
 def extrapolInvolFinal(N,y):
-    Ls=[0.05, 0.1, 0.2, 0.3, 0.4, 0.45, 0.475, 0.485, 0.49]
-    Ls2=np.array([0.1, 0.2, 0.3, 0.4])
-    extrapolInfvol(N,y,Ls,1)
-    MSs=extrapolInfvol(N,y,Ls2)
+    Ls=[0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25, 0.3, 0.35, 0.4, 0.45]#, 0.475, 0.485]
+    Ls2=np.array([ 0.125, 0.15, 0.175, 0.2, 0.225, 0.25, 0.275, 0.3, 0.325, 0.35, 0.375])
+    extrapolInfvol(N,y,Ls,1,0)
+    MSs=extrapolInfvol(N,y,Ls2,0,1)
+    plt.plot(Ls2,MSs )
     p=np.polyfit(Ls2-0.25,np.array(MSs)-0.125,3)
+    #print(p)
     yfit=np.polyval(p,x-0.25)
-    plt.plot(x,yfit+0.125)
-    print(np.polyval(p,-0.25)+0.125)
-
+    plt.plot(x,yfit+0.125, color=colors[i])
+    
+    #print(y*(np.polyval(p,-0.25)+0.125))
+    print("\""+str(N)+"_"+str(y)+"_"+str(0)+"\":", (np.polyval(p,-0.25)+0.125)*y, ",")
+    print("\""+str(N)+"_"+str(y)+"_"+str(0.5)+"\":", (np.polyval(p,0.25)+0.125)*y, ",")
 #for N in range(10,25,4):
 #    extrapolInvolFinal(N,1.0)
 #plt.legend(loc="lower left",handletextpad=-0.5, borderpad=0.4)
 #plt.show()
 #extrapolVolume()
 
-extrapolInvolFinal(24,1.0)
-plt.show()
+
+for eta in range(85,96,5):
+    y=eta/100
+    i=0
+    for N in range(10,24,2):
+        extrapolInvolFinal(N,y)
+        i=i+1
+
+    plt.legend(loc="upper left",handletextpad=-0.5, borderpad=0.4)
+    plt.show()
