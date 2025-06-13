0053e0a867d62a6f9f526735679f2e8717facbf4
ernscht6650 <florian.stein.22@gmx.de>
2025-06-13 18:43:36 +0200

    zentrumsfix

--- Oitmaa-paper/NonDecoOitmaa.py
+++ Oitmaa-paper/NonDecoOitmaa.py
@@ -28,12 +28,13 @@ dict = ast.literal_eval('{'+data+'}')
 def Zentrumshamiltonian(N,y,mdurchg,l):
     mu=2*mdurchg/y
     A=mu*eye_array(2**N)
-    for k in [N/2, N/2+1]:
+    for k in [int(N/2), int(N/2+1)]:
         term=mu/2*(-1)**k*sigmaz
         term2=1/(y**2)*(kron(sigmap,sigmam)+kron(sigmam,sigmap))
         A+=kron(eye_array(2**(k-1)), kron(term, eye_array(2**(N-k))))
         A+=Inter(k,N,l)
-        A+=kron(eye_array(2**(k-1), kron(term2, eye_array(2**(N-k-1)))))
+        #("bussje")
+        A+=kron(eye_array(2**(k-1)), kron(term2, eye_array(2**(N-k-1))))
     return A
 
 #Terme des Hamiltonians
@@ -367,7 +368,7 @@ def GrundzustandsenergieZentrumVol(N,Vol,mdurchg,alpha=0):
             y=Vol/N
             mu2=2*(mdurchg-RenormierungVol(Vol,N,alpha))/y
             omega0=linalg.eigs(NonZeroSpin_entferner(V(N)/(y**2)+WL(N,alpha)+mu2*MassTerm(N),N), k=1, which='SR', return_eigenvectors=True)
-            omega=0.5*np.real(Herm(omega0[1][:,0])@NonZeroSpin_entferner(Zentrumshamiltonian(N,Vol/N, mdurchg, alpha),N)@omega0[1][:,0])
+            omega=0.5*np.real(Herm(omega0[1][:,0])@NonZeroSpin_entferner(Zentrumshamiltonian(N,Vol/N, mdurchg-RenormierungVol(Vol,N,alpha), alpha),N)@omega0[1][:,0])
             return np.real(omega)
 
 
